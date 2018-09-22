b5427e4ad7ad07881629ce2a82a27f89ee93e068
Ilya Kaysin <demarkok@gmail.com>
2018-09-22 19:08:56 +0300

    edit codestyle

--- auxiliary_functions.rkt
+++ auxiliary_functions.rkt
@@ -5,44 +5,38 @@
 ; for environment
 (define st-lookup dict-ref)
 (define st-bound? dict-has-key?)
-(define st-set
-  (lambda (st x e)
-      (dict-set st x (cons 'quote (list e)))))
+(define (st-set st x e)
+  (dict-set st x (cons 'quote (list e))))
 (define st-empty  #hash())
-(define initial-st
-  (lambda (vars d)
-    (if (equal? (length vars) (length d))
-        (for/fold ([st st-empty])
-                  ([i vars]
-                   [j d])
-          (st-set st i j))
-        (error "initial-st error: program arity mismatch"))))
+(define (initial-st vars d)
+  (if (equal? (length vars) (length d))
+      (for/fold ([st st-empty])
+                ([i vars]
+                 [j d])
+        (st-set st i j))
+      (error "initial-st error: program arity mismatch")))
 
 ; for basic_blocks
 (define bb-lookup dict-ref)
 (define bb-set    dict-set)
 (define bb-empty  #hash())
-(define initial-prog
-  (lambda (p)
-    (for/fold ([bbs bb-empty])
-              ([i (cdr p)])
-      (bb-set bbs (car i) (cdr i)))))
+(define (initial-prog p)
+  (for/fold ([bbs bb-empty])
+            ([i (cdr p)])
+    (bb-set bbs (car i) (cdr i))))
 
 ; my-eval
 (define-namespace-anchor a)
 (define ns (namespace-anchor->namespace a))
-(define my-eval
-  (lambda (e)
-    (eval e ns)))
+(define (my-eval e)
+  (eval e ns))
 
 ; eval expression in current environment
-(define subst
-  (lambda (st e)
-    (match e
-      [`(,x . ,y) `(,(subst st x) . ,(subst st y))]
-      [`,x (if (st-bound? st x) (st-lookup st x) x)]
-      )))
-(define eval-exp
-  (lambda (st e)
-    (let ([ee (subst st e)])
-      (my-eval ee))))
+(define (subst st e)
+  (match e
+    [`(,x . ,y) `(,(subst st x) . ,(subst st y))]
+    [`,x (if (st-bound? st x) (st-lookup st x) x)]))
+
+(define (eval-exp st e)
+  (define ee (subst st e))
+  (my-eval ee))
