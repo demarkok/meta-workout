d3c283b4473ef3ed01de4164b8d38516d71b1352
Ilya Kaysin <demarkok@gmail.com>
2018-10-08 01:41:54 +0300

    add tests

--- examples.rkt
+++ examples.rkt
@@ -2,8 +2,13 @@
 
 (require "int.rkt")
 (require "auxiliary_functions.rkt")
+(require "mix.rkt")
 
-(define find_name
+
+(define (ok? expected actual) 
+  (if (equal? expected actual) `OK `(WA. expected: ,expected found: ,actual)))
+
+(define find-name
   '((read name namelist valuelist)
    (search (if (equal? name (car namelist)) found cont))
    (cont (:= valuelist (cdr valuelist))
@@ -11,22 +16,35 @@
          (goto search))
    (found (return (car valuelist)))))
 
-; (int-assn (st-set st-empty 'x '(1 2 3)) '(:= x (car x)))
-(int find_name '(x (x y z) (1 2 3)))
-(int find_name '(z (x y z) (1 2 3)))
-(int int-TM '(((0 if 0 goto 3) (1 right) (2 goto 0) (3 write 1)) 
-              (1 1 0 1 1 0 1)))
+(define tm-simple '((0 if 0 goto 3) (1 right) (2 goto 0) (3 write 1)))
 
 ; binary +1 
-(define inc '((0 if 1 goto 4) 
-              (100 goto 3) 
-              (4 write 0) 
-              (1 right) 
-              (2 goto 0) 
-              (3 write 1)
-              (10 if B goto 13) 
-              (11 left)
-              (12 goto 10)
-              (13 right)))
+(define tm-inc '((0 if 1 goto 4) 
+                (100 goto 3) 
+                (4 write 0) 
+                (1 right) 
+                (2 goto 0) 
+                (3 write 1)
+                (10 if B goto 13) 
+                (11 left)
+                (12 goto 10)
+                (13 right)))
 
-(int int-TM (cons inc `((1 1 1 1 0 0 1))))
+; find-name with specialised name and namelist
+(define spec-find-name (int mix `(,find-name (valuelist) ((name namelist)  (foo (bar foo))))))
+; test find-name specialization
+(ok? 239 (int spec-find-name `((42 239))))
+
+; first futamura projection for int-TM. Compiles tm-simple from Turing Machine into FlowChart
+(define futamura1-simple (int mix `(,int-TM
+                                   (Right Left)
+                                   ((q) (,tm-simple)))))
+; test first futamura projection for tm-simple
+(ok? `(1 1 1 0 1) (int futamura1-simple `((1 1 0 1 1 0 1))))
+
+; first futamura projection for int-TM. Compiles tm-inc from Turing Machine into FlowChart
+(define futamura1-inc (int mix `(,int-TM
+                                (Right Left)
+                                ((q) (,tm-inc)))))
+; test first futamura projection for tm-inc
+(ok? `(0 0 1 1 1 0 1) (int futamura1-inc `((1 1 0 1 1 0 1))))
